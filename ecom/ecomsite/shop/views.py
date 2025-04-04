22a113dd1ecb3fc0261c8263003bcefa6013fa1f
Sai <a.sairam0209@gmail.com>
2025-04-04 21:59:58 +0530

    Adding till Pagination
    will have to add detailed View(Future)

--- ecom/ecomsite/shop/views.py
+++ ecom/ecomsite/shop/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
     products = Product.objects.all()
+
+#   Code for Search
+    item_name = request.GET.get('item_search')
+    if item_name != "" and item_name is not None:
+        print(item_name)
+        products = Product.objects.filter(title__icontains=item_name)
+
+#   Code for Pagination
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
     return render(request, 'shop/index.html', {'products': products})
